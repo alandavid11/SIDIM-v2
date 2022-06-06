ab02e1b190fa853b6035dc4445aca6c91b7ae09d
Alan Gloria <102493628+alandavid11@users.noreply.github.com>
2022-06-06 17:07:26 -0500

    Update database.js
    Comprobacion de conexion

--- src/database.js
+++ src/database.js
@@ -4,20 +4,32 @@ const sqlConfig = {
     user: 'sa',
     password: 'Password1',
     database: 'puntosBorgWarnerRam',
-    server: 'l10.42.6.29',
-    pool: {
+    server: '10.42.6.29',
+    /* pool: {
       max: 10,
       min: 0,
       idleTimeoutMillis: 30000
-    },
+    }, */
     options: {
-      encrypt: true, // for azure
-      trustServerCertificate: false // change to true for local dev / self-signed certs
+      encrypt: false, // for azure
+      trustServerCertificate: false, // change to true for local dev / self-signed certs
+      trustedconnection: true,
     }
   }
 
 function getConnection(){
-    return sqlConfig;
-}
+  try {
+    return sql.connect(sqlConfig);
+  } catch (error) {
+    console.log(error)
+  }
+
+};
+
+setTimeout(async() => {
+  const conn = await getConnection();
+  const resultado = await conn.request().query('SELECT * from usuarios');
+  console.log(resultado);
+}, 1500);
 
 module.exports = { getConnection }
